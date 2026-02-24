904ab20f7025bede4332a1fd30d72c8ca9b51a59
OPDinbo1135 <alexgameeeeeee@gmail.com>
2026-02-24 17:50:17 +0300

    Update test_sample.py
    опечатка

--- test_sample.py
+++ test_sample.py
@@ -1,4 +1,4 @@
 from main import greet
 def test_greet():
  black_list = ['Petya', 'Vanya', 'Masha']
- assert greet("world", balck_list) == "Hello, world!"
+ assert greet("world", black_list) == "Hello, world!"
